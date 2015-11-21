82fb602834c2378821970721b105e37fe2f1412b
Tamandeep Kaur <tamandeep.kaur26@gmail.com>
2015-11-21 15:34:38 -0600

    Process data class

--- TheCancerSpecialist/src/RProcessor.java
+++ TheCancerSpecialist/src/RProcessor.java
@@ -1,4 +1,30 @@
+import rcaller.RCaller;
+import rcaller.RCode;
 
 public class RProcessor {
+	
+	public static void processData()
+	{
+		try {
+  	      RCaller caller = new RCaller();
+  	      caller.setRscriptExecutable("C:/Program Files/R/R-3.2.2/bin/Rscript");
+
+  	      RCode code = new RCode();
+  	      code.clear();
+
+  	      code.R_source("logistic.R");
+  	      
+  	      caller.setRCode(code);
+  	      caller.runAndReturnResult("acc");
+  	      double[] results = caller.getParser().getAsDoubleArray("acc");
+  	      System.out.println("acc is " + results[0]);
+  	      
+  	      caller.getCranRepos();
+  	      //code.showPlot(file);
+  	    } catch (Exception e) {
+  	     e.printStackTrace();
+  	    }
+	 
+	    }
 	
 }
