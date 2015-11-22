03ff8cb4fb853d4382887866f60eb1d32252db23
Tamandeep Kaur <tamandeep.kaur26@gmail.com>
2015-11-21 18:33:18 -0600

    All classifiers called

--- TheCancerSpecialist/src/RProcessor.java
+++ TheCancerSpecialist/src/RProcessor.java
@@ -4,35 +4,47 @@ import rcaller.RCode;
 public class RProcessor {
 	
 	public static void processData(){
+		testAllClassifiers("Dtree.R");
+		testAllClassifiers("Bagging.R");
+		testAllClassifiers("Boosting.R");
+		testAllClassifiers("KNN.R");
+		testAllClassifiers("logistic.R");
+		testAllClassifiers("NaiveBayes.R");
+		testAllClassifiers("neuralNet.R");
+		testAllClassifiers("RandomForest.R");
+		testAllClassifiers("SVMperceptron.R");
+	}
+	public static void testAllClassifiers(String file)
+	{
 		try {
-    		
-  		  //Create an object to link java to R
-  	      RCaller caller = new RCaller();
-  	      caller.setRscriptExecutable("C:/Program Files/R/R-3.2.2/bin/Rscript");
-  	      
-  	      //creating a code object to compile in R
-  	      RCode code = new RCode();
-  	      //Clear previous code assignments to the code object
-  	      //not necessary here but should be used later on
-  	      code.clear();
+    		System.out.println(file);
+	  		  //Create an object to link java to R
+	  	      RCaller caller = new RCaller();
+	  	      caller.setRscriptExecutable("C:/Program Files/R/R-3.2.2/bin/Rscript");
+	  	      
+	  	      //creating a code object to compile in R
+	  	      RCode code = new RCode();
+	  	      //Clear previous code assignments to the code object
+	  	      //not necessary here but should be used later on
+	  	      code.clear();
 
-  	      //Get the code from a R code file created
-  	      code.R_source("RandomForest.R");
-  	      
-  	      //set code to the Rcaller object
-  	      caller.setRCode(code);
-  	      //get the result from return list with key = acc
-  	      caller.runAndReturnResult("acc");
-  	      
-  	      //extract the result array
-  	      double[] results = caller.getParser().getAsDoubleArray("acc");
-  	      
-  	      System.out.println("acc is " + results[0]*100 + "%");
-  	      
-  	      caller.getCranRepos();
-  	     
-  	    } catch (Exception e) {
-  	     e.printStackTrace();
-  	    }
+	  	      //Get the code from a R code file created
+	  	      code.R_source(file);
+	  	      
+	  	      //set code to the Rcaller object
+	  	      caller.setRCode(code);
+	  	      //get the result from return list with key = acc
+	  	      caller.runAndReturnResult("acc");
+	  	      
+	  	      //extract the result array
+	  	      double[] results = caller.getParser().getAsDoubleArray("acc");
+	  	      
+	  	      System.out.println("Accuracy is " + results[0]*100 + "%");
+	  	      
+	  	      caller.getCranRepos();
+	  	     
+	  	    } catch (Exception e) {
+	  	     e.printStackTrace();
+	  	    }
 	}
 }
